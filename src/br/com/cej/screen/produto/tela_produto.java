87f51fd2658c5c940ee2ddf36a16ea22d88a23f9
Edgar Gavioli <58188033+edgargavioli@users.noreply.github.com>
2023-11-07 22:06:13 -0300

    Add files via upload

---
+++ src/br/com/cej/screen/produto/tela_produto.java
@@ -0,0 +1,16 @@
+package br.com.cej.screen.produto;
+
+import javax.swing.*;
+
+public class tela_produto {
+    private JPanel mainPanel;
+    private JTextField descricao;
+    private JTextField unidade_d_medida;
+    private JTextField quantidade_minima;
+    private JTextField preco_d_compra;
+    private JTextField preco_d_venda;
+    private JButton Criar;
+    private JButton returnButton;
+
+
+}
